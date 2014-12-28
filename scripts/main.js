24d3dab7401b3948689006cc7a2e5a41e745ebbc
Jasper Forest <jasper@liaapp.com>
2014-12-27 18:40:43 -0800

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,9 @@
 var menu_items = ["Contact", "Github", "Projects", "Resume", "Jasper Forest Web Developer"];
 
 function initLoad(){
-	
 	for (var i = menu_items.length - 1; i >= 0; i--) {
-		document.getElementById('menu').innerHTML += "<li>\n" + menu_items[i] + "\n</li>";
+		var item ="<li>\n" + menu_items[i] + "\n</li>";
+		document.getElementById('menu').innerHTML += item; 
 	};
+
 }
