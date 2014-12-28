2b47844d55f045692612168e4676b6d5b7923917
Jasper Forest <jasper@liaapp.com>
2014-12-27 22:43:38 -0800

    fix

--- scripts/main.js
+++ scripts/main.js
@@ -20,13 +20,7 @@ function initLoad(){
 		var menu_name = menu_items[i];
 		var item;
 		if (menu_name.search("Github") != -1) {
-			item = "<a href=\"https://github.com/JPERF\"" 
-
-			if(url.search(menu_items[i] != 1)){
-				items += "style=\"background-color:black; color:white;\"";
-			}
-
-			item += ">\n" + "<li class=\"menu_items\">\n"
+			item = "<a href=\"https://github.com/JPERF\">\n" + "<li class=\"menu_items\">\n"
 					+ menu_name + "\n</li>"+ "\n</a>";
 		}
 		else if(menu_name.search("Jasper Forest - Web Developer") != -1){
@@ -34,7 +28,13 @@ function initLoad(){
 			 +  menu_name  + "\n</li>"+ "\n</a>";
 		}
 		else{
-			item ="<a href=\"#/" + menu_name + "\">\n" + "<li class=\"menu_items\">\n" 
+			item ="<a href=\"#/" + menu_name + "\"";
+
+			if(url.search(menu_items[i] != 1)){
+				items += "style=\"background-color:black; color:white;\"";
+			}
+
+			items += ">\n" + "<li class=\"menu_items\">\n" 
 					+ menu_name  + "\n</li>"+ "\n</a>";
 		}
 		document.getElementById('menu').innerHTML += item; 
