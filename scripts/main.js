9d9dd78717661174fe478a87ff518cb73750bb66
Jasper Forest <jasper@liaapp.com>
2014-12-27 19:05:45 -0800

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,20 @@
 // Main javascript 
 
-var menu_items = ["Contact", "Github", "Projects", "Resume", "Jasper Forest - Web Developer"];
+var menu_items = ["Contact", "Github", "Projects",
+				 "Resume", "Jasper Forest - Web Developer"];
+
+var languages_libraries = ["Java", "JavaScript", "C", "Swift",
+				"OpenGL", "Python", "CSS", "HTML", "Git"];
 
 function initLoad(){
+
+	// Load top menu
 	for (var i = menu_items.length - 1; i >= 0; i--) {
 		var item ="<li class=\"menu_items\">\n" + menu_items[i] + "\n</li>";
 		document.getElementById('menu').innerHTML += item; 
 	};
+
+	var url = document.URL;
+	alert(url);
 
 }
