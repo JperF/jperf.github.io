7476980fc927e7aebfd30c28dc058cbd21e67b67
Jasper Forest <jasper@liaapp.com>
2014-12-27 18:38:15 -0800

    css

--- scripts/main.js
+++ scripts/main.js
@@ -7,5 +7,4 @@ function initLoad(){
 	for (var i = menu_items.length - 1; i >= 0; i--) {
 		document.getElementById('menu').innerHTML += "<li>\n" + menu_items[i] + "\n</li>";
 	};
-	alert("loaded");
 }
