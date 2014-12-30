0766d97de3624f40f3db90e5c3f679463a8dd6e7
Jasper Forest <jasper@liaapp.com>
2014-12-29 20:44:30 -0800

    fix

--- scripts/main.js
+++ scripts/main.js
@@ -29,7 +29,8 @@ function initLoad(){
 		else{
 			item ="<a href=\"#/" + menu_name + "\"";
 			alert(url)
-			if(url.search(menu_items[i]) != -1 || (url.valueOf() == "jperf.github.io/" && menu_items[i].valueOf() == "Resume")){
+			if(url.search(menu_items[i]) != -1 || 
+				(url.valueOf() == "http://jperf.github.io/" && menu_items[i].valueOf() == "Resume")){
 			 	item += ">\n <li style=\"background-color:black; color:white;\"";
 			}
 			else{
