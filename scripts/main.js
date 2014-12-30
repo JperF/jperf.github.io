ff210aafa62c3b0d953a62796f0ba74f3ea7d13c
Jasper Forest <jasper@liaapp.com>
2014-12-29 22:54:07 -0800

    qwe

--- scripts/main.js
+++ scripts/main.js
@@ -86,7 +86,7 @@ function listCreator(list){
 		string += "<li";
 
 		if(i % 3 == 0){
-			string +=" style=\"display:inline\";";
+			string +=" style=\"display:none\";";
 		}		
 
 		string += " class=\"languages\">" ;
