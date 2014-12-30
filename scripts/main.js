63d12c1e0a1d8045267310299e90281a255b0089
Jasper Forest <jasper@liaapp.com>
2014-12-29 22:20:17 -0800

    rwe

--- scripts/main.js
+++ scripts/main.js
@@ -80,7 +80,7 @@ function listCreator(list){
 		rowNum = 3;
 	}
 
-	string += "<ul style=\"list-style-type:none;\">";
+	string += "<ul style=\"list-style-type:disc;\">";
 	for (var i = 0; i <= len-1; i++) {
 
 		string += "<li";
